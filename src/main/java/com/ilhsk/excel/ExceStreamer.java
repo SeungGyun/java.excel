4b76e64a55377de5c50543c02a5406c0975f2e4d
ilhsk <ilhsk@nm-neo.com>
2021-09-13 18:56:02 +0900

    [홍승균] 대용량 엑셀 파일 읽을시 메모리 오류 해결

---
+++ src/main/java/com/ilhsk/excel/ExceStreamer.java
@@ -0,0 +1,31 @@
+
+package com.ilhsk.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import com.monitorjbl.xlsx.StreamingReader;
+
+
+public class ExceStreamer {
+
+	public static void main(String[] args) throws Exception {
+		try (InputStream is = new FileInputStream(new File("d:/tt.xlsx")); Workbook workbook = StreamingReader.builder().rowCacheSize(100).bufferSize(4096).open(is)) {
+			for (Sheet sheet : workbook) {
+				System.out.println(sheet.getSheetName());
+				for (Row r : sheet) {
+					for (Cell c : r) {
+						System.out.println(c.getStringCellValue());
+					}
+				}
+			}
+		}
+	}
+
+}
